cb5f62892794d4a6852142736237e41b8a35d732
Maria Rosa <mariarosagennarelli@hotmail.com>
2023-03-14 21:07:10 +0100

    Exercise Email 1

---
+++ Email 1/src/main/java/co/develhope/email1/students/services/StudentService.java
@@ -0,0 +1,25 @@
+package co.develhope.email1.students.services;
+
+import org.springframework.stereotype.Service;
+import co.develhope.email1.students.entities.Student;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    static List<Student> students = Arrays.asList(
+            new Student("1", "Aldo", "Baglio", "aldo@baglio.com"),
+            new Student("2", "Giovanni", "Storti", "giovanni@storti.com"),
+            new Student("3", "Giacomo", "Poretti", "giacomo@poretti.com"),
+            new Student("4", "Treuomini", "Edunagamba", "treuomini@edunagamba.eu")
+            );
+
+    public Student getStudentById(String studentId) {
+        Optional <Student> studentFromList = students.stream().filter(student ->
+                student.getId().equals(studentId)).findAny();
+        return studentFromList.isPresent() ? studentFromList.get() : null;
+    }
+}
